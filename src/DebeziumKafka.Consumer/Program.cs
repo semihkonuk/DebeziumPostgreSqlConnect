2407b3c615216b5d0e4629c5f32e47c0236205df
Semih <semih.konuk@outlook.com>
2022-03-31 20:36:12 +0300

    Add project files.

---
+++ src/DebeziumKafka.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+try
+{
+    Console.WriteLine("Started...");
+    var config = new ConsumerConfig { GroupId = "exampledb-consumer", BootstrapServers = "kafka:9092" };
+
+    using (var consumer = new ConsumerBuilder<string,string>(config).Build())
+    {
+        consumer.Subscribe("postgres.public.employee");
+
+        while (true)
+        {
+            ConsumeResult<string, string> consumeResult = consumer.Consume();
+            Console.WriteLine($"Name of topic {consumeResult.TopicPartitionOffset} message :{consumeResult.Message.Value}");
+            consumer.Commit();
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
